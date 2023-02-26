ab331f93f1acb981b41b4108e8edb9cf6c35d8d1
Mike Repec <mikerepec@gmail.com>
2023-02-27 00:52:55 +0100

    Simplify isLinked check in BinaryTreeTest

--- src/test/kotlin/com/github/flexicon/mazes/algorithms/BinaryTreeTest.kt
+++ src/test/kotlin/com/github/flexicon/mazes/algorithms/BinaryTreeTest.kt
@@ -1,5 +1,6 @@
 package com.github.flexicon.mazes.algorithms
 
+import com.github.flexicon.mazes.Cell
 import com.github.flexicon.mazes.Grid
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
@@ -12,15 +13,18 @@ class BinaryTreeTest {
         val grid = Grid(3, 3)
         BinaryTree(rand).applyTo(grid)
 
-        assert(grid[0, 0]!!.isLinked(grid[0, 1]))
-        assert(grid[0, 1]!!.isLinked(grid[0, 2]))
+        assert(grid[0, 0].isLinked(grid[0, 1]))
+        assert(grid[0, 1].isLinked(grid[0, 2]))
 
-        assert(grid[1, 0]!!.isLinked(grid[1, 1]))
-        assert(grid[1, 1]!!.isLinked(grid[0, 1]))
-        assert(grid[1, 2]!!.isLinked(grid[0, 2]))
+        assert(grid[1, 0].isLinked(grid[1, 1]))
+        assert(grid[1, 1].isLinked(grid[0, 1]))
+        assert(grid[1, 2].isLinked(grid[0, 2]))
 
-        assert(grid[2, 0]!!.isLinked(grid[2, 1]))
-        assert(grid[2, 1]!!.isLinked(grid[2, 2]))
-        assert(grid[2, 2]!!.isLinked(grid[1, 2]))
+        assert(grid[2, 0].isLinked(grid[2, 1]))
+        assert(grid[2, 1].isLinked(grid[2, 2]))
+        assert(grid[2, 2].isLinked(grid[1, 2]))
     }
+
+    private fun Cell?.isLinked(other: Cell?) =
+        this?.isLinked(other) ?: false
 }
