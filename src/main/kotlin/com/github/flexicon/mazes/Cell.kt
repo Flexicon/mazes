5f2348f5c3d22fa545d446df6dc383b71241bf9d
Mike Repec <mikerepec@gmail.com>
2023-02-26 16:26:19 +0100

    Initial Cell implementation
    Contains mutability for now. Will work on this later.

---
+++ src/main/kotlin/com/github/flexicon/mazes/Cell.kt
@@ -0,0 +1,29 @@
+package com.github.flexicon.mazes
+
+data class Cell(val row: Int, val column: Int) {
+    private val links = mutableSetOf<Cell>()
+    var north: Cell? = null
+    var east: Cell? = null
+    var south: Cell? = null
+    var west: Cell? = null
+
+    fun link(cell: Cell, bidi: Boolean = true): Cell = apply {
+        links.add(cell)
+        if (bidi) cell.link(this, bidi = false)
+    }
+
+    fun unlink(cell: Cell, bidi: Boolean = true): Cell = apply {
+        links.remove(cell)
+        if (bidi) cell.unlink(this, bidi = false)
+    }
+
+    fun isLinked(cell: Cell) = links.contains(cell)
+
+    val neighbors: Set<Cell>
+        get() = buildSet {
+            north?.let { add(it) }
+            east?.let { add(it) }
+            south?.let { add(it) }
+            west?.let { add(it) }
+        }
+}
---
+++ src/test/kotlin/com/github/flexicon/mazes/CellTest.kt
@@ -0,0 +1,54 @@
+package com.github.flexicon.mazes
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CellTest {
+
+    @Test
+    fun `should link and unlink cells`() {
+        val cell1 = Cell(1, 1)
+        val cell2 = Cell(1, 2)
+
+        assert(!cell1.isLinked(cell2))
+        assert(!cell2.isLinked(cell1))
+
+        cell1.link(cell2)
+        assert(cell1.isLinked(cell2))
+        assert(cell2.isLinked(cell1))
+
+        cell1.unlink(cell2)
+        assert(!cell1.isLinked(cell2))
+        assert(!cell2.isLinked(cell1))
+    }
+
+    @Test
+    fun `should return appropriate neighbors`() {
+        val cell = Cell(2, 3)
+        assertEquals(emptySet(), cell.neighbors)
+
+        val nCell = Cell(3, 3)
+        cell.north = nCell
+        assertEquals(setOf(nCell), cell.neighbors)
+
+        val eCell = Cell(2, 4)
+        cell.east = eCell
+        assertEquals(setOf(nCell, eCell), cell.neighbors)
+
+        val sCell = Cell(1, 3)
+        cell.south = sCell
+        assertEquals(setOf(nCell, eCell, sCell), cell.neighbors)
+
+        val wCell = Cell(2, 2)
+        cell.west = wCell
+        assertEquals(setOf(nCell, eCell, sCell, wCell), cell.neighbors)
+
+        val nCell2 = Cell(nCell.row, nCell.column)
+        cell.north = nCell2
+        // nCell == nCell2 -> true which means the assertion doesn't need to change
+        assertEquals(setOf(nCell, eCell, sCell, wCell), cell.neighbors)
+
+        cell.east = null
+        assertEquals(setOf(nCell, sCell, wCell), cell.neighbors)
+    }
+}
