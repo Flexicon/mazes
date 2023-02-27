59dd059c6b176935e93d2a350842865cc8faa963
Mike Repec <mikerepec@gmail.com>
2023-02-27 22:14:37 +0100

    Initial Sidewinder implementation

--- src/main/kotlin/com/github/flexicon/mazes/Application.kt
+++ src/main/kotlin/com/github/flexicon/mazes/Application.kt
@@ -5,4 +5,7 @@ import com.github.flexicon.mazes.transformers.toAscii
 fun main() {
     val btree = Maze.binaryTree(10, 10)
     println(btree.toAscii())
+
+    val sidewinder = Maze.sidewinder(10, 10)
+    println(sidewinder.toAscii())
 }
--- src/main/kotlin/com/github/flexicon/mazes/algorithms/Sidewinder.kt
+++ src/main/kotlin/com/github/flexicon/mazes/algorithms/Sidewinder.kt
@@ -1,12 +1,31 @@
 package com.github.flexicon.mazes.algorithms
 
+import com.github.flexicon.mazes.Cell
 import com.github.flexicon.mazes.Grid
 import kotlin.random.Random
 
 class Sidewinder internal constructor(private val rand: Random = Random) : Algorithm {
     override fun applyTo(grid: Grid) {
-        TODO("Not yet implemented")
+        grid.forEachRow { row ->
+            val cellsRun = mutableListOf<Cell>()
+
+            row.forEach { cell ->
+                cellsRun.add(cell)
+
+                if (shouldCloseOutRun(cell)) {
+                    cellsRun.apply {
+                        random(rand).apply { north?.link(this) }
+                        clear()
+                    }
+                } else {
+                    cell.east?.let { cell.link(it) }
+                }
+            }
+        }
     }
+
+    private fun shouldCloseOutRun(cell: Cell): Boolean =
+        cell.east == null || (cell.north != null && rand.nextBoolean())
 
     companion object : Algorithm by Sidewinder()
 }
